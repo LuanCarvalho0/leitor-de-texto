903d9a384c2e38942d2f1a14ab3b8f623bd18f37
LuanCarvalho0 <luan_carvalho1997@hotmail.com>
2023-01-19 19:31:29 -0300

    correção de erro

--- conversor.py
+++ conversor.py
@@ -16,22 +16,23 @@ print('-' * 30)
 print('[1] - Escolha um arquivo existente\n[2] - Digite o texto a ser convertido')
 opcao = input('Escolha uma opção: ')
 
-try:
-    if opcao == '1':
-        nome_arquivo = input('Digite o nome do aquivo: ')
-        texto = ''
-        with open(f'{nome_arquivo}.txt', 'r', encoding='utf-8') as arquivo:
-            for linha in arquivo:
-                texto += linha
-        print(texto)
-        converter_txt_em_audio(nome_arquivo, texto)
-    elif opcao == '2':
-        nome_arquivo = input('Digite o nome do aquivo: ')
-        texto = input('Digite o texto: ')
-        converter_txt_em_audio(nome_arquivo, texto)
-except:
+
+if opcao == '1':
+    nome_arquivo = input('Digite o nome do aquivo: ')
+    texto = ''
+    with open(f'{nome_arquivo}.txt', 'r', encoding='utf-8') as arquivo:
+        for linha in arquivo:
+            texto += linha
+    print(texto)
+    converter_txt_em_audio(nome_arquivo, texto)
+elif opcao == '2':
+    nome_arquivo = input('Digite o nome do aquivo: ')
+    texto = input('Digite o texto: ')
+    converter_txt_em_audio(nome_arquivo, texto)
+else:
     print('Opção invalida!')
     print('Programa Encerrado...')
+    exit()
 
 print('Tocando...')
 playsound(f'{nome_arquivo}.mp3')
